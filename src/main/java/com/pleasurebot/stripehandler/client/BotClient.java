9692a99935da3b55bb3f7298eaf2d9534598a306
prost <vlad.reut@outlook.com>
2023-02-10 20:58:06 +0100

    logging and profiles

--- src/main/java/com/pleasurebot/stripehandler/client/BotClient.java
+++ src/main/java/com/pleasurebot/stripehandler/client/BotClient.java
@@ -3,7 +3,6 @@ package com.pleasurebot.stripehandler.client;
 import com.pleasurebot.stripehandler.model.CreateUserRequest;
 import com.pleasurebot.stripehandler.model.CreateUserResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
